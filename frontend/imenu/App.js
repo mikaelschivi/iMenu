1484c6f3ce473834c92acf53d715313299c4e925
Vinícius Martins Borges <vinimborges@gmail.com>
2023-11-12 23:08:31 -0300

    Alterações na tela de admin

--- frontend/imenu/App.js
+++ frontend/imenu/App.js
@@ -6,6 +6,10 @@ import Prato from './src/Componentes/Prato';
 import Inform from './src/Componentes/Inform';
 import Adm from './src/Componentes/Adm';
 import Initial from "./src/Componentes/Initial";
+import Add from "./src/Componentes/Add";
+import Verify from "./src/Componentes/Verify";
+import Delete from "./src/Componentes/Delete";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -38,6 +42,21 @@ function MyStack() {
           component={Inform}
           options={{ statusBarColor:"#277C9D"}}>
         </Stack.Screen> 
+        <Stack.Screen 
+          name="Add"
+          component={Add}
+          options={{ statusBarColor:"#277C9D"}}>
+        </Stack.Screen>
+        <Stack.Screen 
+          name="Verify"
+          component={Verify}
+          options={{ statusBarColor:"#277C9D"}}>
+        </Stack.Screen>
+        <Stack.Screen 
+          name="Delete"
+          component={Delete}
+          options={{ statusBarColor:"#277C9D"}}>
+        </Stack.Screen>
       </Stack.Navigator>
   );
 }
---
+++ frontend/imenu/src/Componentes/Verify.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View , Text} from 'react-native';
+
+export default function Delete () {
+    return (
+        <View style={{alignItems: "center", marginTop:'60%'}}>      
+            <View>
+                <Text style={{fontSize:18}}>Essa é a tela para verificar o banco de lanches</Text>
+                
+            </View>
+        </View>
+    );
+}
